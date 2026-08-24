31831ffa0a42a69014123314a430cc153ef7be66
cico <nicocidips@gmail.com>
2026-08-24 20:11:49 -0300

    Agregar tests provistos por la catedra

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("junit:junit:4.12")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.13.4")
 }
 
 tasks.test {
---
+++ resources/sinErrores/lexSinErrores01.java
@@ -0,0 +1,21 @@
+class Alpha {
+    var texto = "hola, mundo";
+    var letra = '\n';
+}
+
+//#class,1
+//#Alpha,1
+//#{,1
+//#var,2
+//#texto,2
+//#=,2
+//#"hola, mundo",2
+//#;,2
+//#var,3
+//#letra,3
+//#=,3
+//#'\n',3
+//#;,3
+//#},4
+//#$,22
+
