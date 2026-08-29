a7a0cbcd824199eaa4255bc118bab2c239e96a00
cico <nicocidips@gmail.com>
2026-08-29 04:44:58 -0300

    Agregar tests de codigo MINIJAVA simulado

---
+++ resources/sinErrores/lexSinErrores_metodos.java
@@ -0,0 +1,47 @@
+// SinErrores
+class Prueba2 {
+    char c = 'x';
+    boolean b = true;
+    String s = "hola mundo";
+
+    int suma(int x, int y) {
+        return x + y;
+    }
+}
+
+//#class,2
+//#Prueba2,2
+//#{,2
+//#char,3
+//#c,3
+//#=,3
+//#'x',3
+//#;,3
+//#boolean,4
+//#b,4
+//#=,4
+//#true,4
+//#;,4
+//#String,5
+//#s,5
+//#=,5
+//#"hola mundo",5
+//#;,5
+//#int,7
+//#suma,7
+//#(,7
+//#int,7
+//#x,7
+//#,,7
+//#int,7
+//#y,7
+//#),7
+//#{,7
+//#return,8
+//#x,8
+//#+,8
+//#y,8
+//#;,8
+//#},9
+//#},10
+//#$,48
