287bca380aaa5133c5d9ae79aa095a44c281df84
cico <nicocidips@gmail.com>
2026-09-18 21:56:22 -0300

    Finalizar implementacion de operador ternario

--- resources/sintactico/NicolasCid/conErrores/67_ternario_falta_punto_y_coma.java
+++ resources/sintactico/NicolasCid/conErrores/67_ternario_falta_punto_y_coma.java
@@ -1,5 +1,5 @@
-///[Error:|8]
-// Falta ";" al final de la sentencia con ternario: se llega a EOF
+///[Error:}|7]
+// Falta ";" al final de la sentencia con ternario: el error se detecta al llegar al "}" del bloque
 
 class Aa {
     void metodo() {
